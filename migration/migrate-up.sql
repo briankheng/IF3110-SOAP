aa2f6ede19560287b3763db430b761323f5466cf
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-11-01 15:33:27 +0700

    feat : setup the subscriptions table, model, and repo

--- migration/migrate-up.sql
+++ migration/migrate-up.sql
@@ -16,3 +16,9 @@ CREATE TABLE `logging` (
 INSERT INTO api_keys VALUES
 ('1a5b0c4d8e1f8f6a9b0e0b0a6c9f4e4d3c9i8b3a7f6f0d5g4h9i6j8b3c9o0p8b', 'rest service'),
 ('7a3f6f0d5g4h9i6j8b3c9o0p8b5e9f3f1d6a81a5b0c4d8e1f8f6a9b0e0b0a6c9', 'php app service');
+
+CREATE TABLE `subscriptions` (
+  `user_id` int NOT NULL,
+  `album_id` int NOT NULL,
+  `status` enum('PENDING','REJECTED','ACCEPTED') NOT NULL DEFAULT 'PENDING'
+);
---
+++ src/main/java/models/Subscription.java
@@ -0,0 +1,44 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.xml.bind.annotation.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@XmlRootElement
+public class Subscription extends ModelInterface {
+    private Integer userId;
+    private Integer albumId;
+    private SubscriptionStatus status;
+
+    @XmlEnum(String.class)
+    public enum SubscriptionStatus {
+        PENDING,
+        ACCEPTED,
+        REJECTED;
+
+        public static SubscriptionStatus fromStatusCode(String value) {
+            for (SubscriptionStatus status : SubscriptionStatus.values()) {
+                if (status.toString().equalsIgnoreCase(value)) {
+                    return status;
+                }
+            }
+            return null;
+        }
+    }
+
+    @Override
+    public void constructFromSQL(ResultSet rs) throws SQLException {
+        this.userId = rs.getInt("user_id");
+        this.albumId = rs.getInt("album_id");
+        this.status = Subscription.SubscriptionStatus.fromStatusCode(rs.getString("status"));
+    }
+}
